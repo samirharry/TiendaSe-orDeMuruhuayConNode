6a8e64f1e2a46189e804f68909fc31c8f8e04dbc
Samir Harry Huamán Patricio <samirharry13@gmail.com>
2018-08-21 00:38:53 -0500

    Agregar mas Categorias"

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB ;
 if(process.env.NODE_ENV === 'dev'){
    urlDB = 'mongodb://localhost:27017/tienda';
 } else{
-    urlDB = 'mongodb://samirharry:20164014I@ds125892.mlab.com:25892/tiendasenior';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
