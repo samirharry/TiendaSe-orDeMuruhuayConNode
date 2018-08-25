4569f2f3b60ffb48bb7117eac8900403dea2fc02
Samir Harry Huamán Patricio <samirharry13@gmail.com>
2018-08-25 08:45:26 -0500

    Arreglo del toggle

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,20 @@
 // Check for click events on the navbar burger icon
-$(".navbar-burger").click(function() {
+/* $(".navbar-burger").click(function() {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
-});
+}); */
 // ====================================================
 // Barra de navegacion unica
 // ===============================================
 var navBar = new Vue({
     el: '#navBar',
     methods:{
+        toggleMovil:()=>{
+            console.log('despliegue');
+            $(".navbar-burger").toggleClass("is-active");
+            $(".navbar-menu").toggleClass("is-active");
+        },
         showProductos: ()=>{
             divCat.catSeen = false;
             divMar.marSeen = false;
